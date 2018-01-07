f669f91411ac709e57ad207e64839cd309f1ff6a
Alex J <alexander.aj.ranger@gmail.com>
2018-01-07 18:36:37 +0500

    ReactJS included

---
+++ source/Bearlog.Web/Scripts/test.jsx
@@ -0,0 +1,13 @@
+﻿var CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div className="commentBox">
+                ReactJS greets you, stranger!
+            </div>
+        );
+    }
+});
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);

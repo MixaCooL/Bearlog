b16d52c31a807a8ecc4574d117f9b56069a152e8
Alex J <alexander.aj.ranger@gmail.com>
2018-04-07 14:19:06 +0500

    .

---
+++ source/Bearlog.Web/Entities/Entities.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.SqlServer.Server;
+
+namespace Bearlog
+{
+    public abstract class Translation
+    {
+        public Guid Id { get; set; }
+        public Guid CreatorId { get; set; }
+        public List<Guid> PartIds { get; set; }
+    }
+
+    /// <summary>
+    /// Для книги - главы
+    /// Для стихов - сонеты или типа того
+    /// Для субтитров - серии
+    /// Для визуала - страница
+    /// Для ресурсов - раздел
+    /// и т.д.
+    /// </summary>
+    public class Part
+    {
+        public Guid Id { get;set; }
+        public List<Guid> FragmentIds { get; set; }
+    }
+
+    /// <summary>
+    /// Кусок текста для перевода
+    /// Абзац, предложение или что-то произвольное
+    /// </summary>
+    public class Fragment
+    {
+        public Guid Id { get; set; }
+        public string OriginalText { get; set; }
+        /// <summary>
+        /// Один и тот же кусок может иметь несколько переводов
+        /// Например, от нескольких переводчиков
+        /// </summary>
+        public List<Guid> FragmentTranslationIds { get; set; }
+    }
+
+    public class FragmentTranslation
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public Guid TranslatorId { get; set; }
+        /// <summary>
+        /// За перевод могут проголосовать: хороший или плохой
+        /// Так получится рейтинг
+        /// </summary>
+        public int Rating { get; set; }
+        public List<Guid> CommentIds { get; set; }
+    }
+
+    public class Book : Translation
+    {
+
+    }
+
+    public class Lyric : Translation
+    {
+
+    }
+
+    public class Subtitle : Translation
+    {
+
+    }
+
+    public class Visual : Translation
+    {
+
+    }
+
+    public class Resource : Translation
+    {
+
+    }
+
+    public class User
+    {
+
+    }
+}
--- source/Bearlog.Web/Models/TranslationModels.cs
+++ source/Bearlog.Web/Models/TranslationModels.cs
@@ -11,6 +11,9 @@ namespace Bearlog.Web.Models
     {
         public Guid Id { get; set; }
         public string[] Tags { get; set; }
+        /// <summary>
+        /// Ссылка на User
+        /// </summary>
         public Guid CreatorId { get; set; }
         public string Name { get; set; }
         public string OriginalName { get; set; }
@@ -25,7 +28,50 @@ namespace Bearlog.Web.Models
     {
         public string AuthorName { get; set; }
         public string AuthorOriginalName { get; set; }
+        //public string FromLanguage { get; set; }
+        //public string ToLanguage { get; set; }
         public int Year { get; set; }
         public List<Part> Parts { get; set; }
+    }
+
+   
+    
+    public class Part
+    {
+        public Guid Id { get; set; }
+        public List<Fragment> Fragments { get; set; } // хранится как нумерованный список О_о
+        public string Name;
+        public string OriginalName { get; set; }
+
+    }
+
+    public class Fragment
+    {
+        public Guid Id { get; set; }
+        public string OriginalText;
+        public List<FragmentTranslation> Translations { get; set; }
+    }
+    
+    public class FragmentTranslation{
+        public Guid Id { get; set; }
+        public Guid AuthorId { get; set; }
+        public Guid FragmentId { get; set; }
+        public string Text { get; set; }
+        public int Rating { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreateAt { get; set; }
+        public List<Comments> Comments { get; set; }
+
+    }
+
+    public class Comments
+    {
+        
+    }
+
+    public class Language
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
     }
 }
